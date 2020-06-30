db2e55861ebd645ef973680b76e14453e40a794b
GLEBR1K <9glebr@gmail.com>
2020-06-30 21:27:35 +0300

    Fix webpack mode assignment and minification

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,15 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssnanoPlugin = require('cssnano-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-const dev = process.env.NODE_ENV !== 'production';
-
-module.exports = {
-  mode: dev ? 'development' : 'production',
-  devtool: dev ? 'source-map' : 'none',
+module.exports = (env, argv) => ({
+  devtool: argv.mode !== 'production' ? 'source-map' : 'none',
 
   optimization: {
-    minimizer: [new CssnanoPlugin()],
+    minimizer: [
+      new TerserPlugin({ extractComments: false }),
+      new CssnanoPlugin()],
   },
 
   entry: './src/index.js',
@@ -27,9 +27,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        use: [
-          'babel-loader',
-        ],
+        use: ['babel-loader'],
         exclude: /node_modules/,
       },
       {
@@ -56,4 +54,4 @@ module.exports = {
     host: '0.0.0.0',
     disableHostCheck: true,
   },
-};
+});
