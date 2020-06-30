6ac08dcc9bb9b5d01a0195efe88a39be9a3f67a4
GLEBR1K <9glebr@gmail.com>
2020-06-30 21:02:49 +0300

    Disable code splitting
    Migrate to single bundles for JS and CSS content.
    Resolve #3 FOUC on page load

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
+const webpack = require('webpack');
 const path = require('path');
+
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssnanoPlugin = require('cssnano-webpack-plugin');
 
 const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: dev ? 'development' : 'production',
-  devtool: dev ? 'inline-source-map' : 'source-map',
+  devtool: dev ? 'source-map' : 'none',
+
+  optimization: {
+    minimizer: [new CssnanoPlugin()],
+  },
 
   entry: './src/index.js',
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: 'dist/',
     filename: 'bundle.js',
-    chunkFilename: '[chunkhash].bundle.js',
   },
 
   module: {
@@ -27,16 +34,18 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [ExtractCssChunks.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
 
   plugins: [
     new CleanWebpackPlugin(),
-    new ExtractCssChunks({
+    new MiniCssExtractPlugin({
       filename: 'bundle.css',
-      chunkFilename: '[chunkhash].bundle.css',
+    }),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
     }),
   ],
 
