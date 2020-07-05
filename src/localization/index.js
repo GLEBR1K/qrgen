45d0e947e15097f1758666ced834f8b9f3e218fd
GLEBR1K <9glebr@gmail.com>
2020-07-05 19:34:16 +0300

    GH-13 :earth_americas: German translation

--- src/localization/index.js
+++ src/localization/index.js
@@ -1,10 +1,11 @@
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import translator from './translator';
-import { en, ru } from './locales';
+import { en, de, ru } from './locales';
 
 const resources = {
   en: { translation: en },
+  de: { translation: de },
   ru: { translation: ru },
 };
 
--- src/localization/locales/index.js
+++ src/localization/locales/index.js
@@ -1,7 +1,9 @@
 import en from './en.json';
+import de from './de.json';
 import ru from './ru.json';
 
 export {
   en,
+  de,
   ru,
 };
